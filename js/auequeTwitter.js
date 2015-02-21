e6614a523329e826d3d699cedfa692a3fbd7301e
ekamp <ekamp@eden.rutgers.edu>
2015-02-21 18:43:48 -0500

    Initial Code commit

---
+++ js/auequeTwitter.js
@@ -0,0 +1,8 @@
+// ##### Simple query to get Post information from Twitter #####
+//id is the blog id collected from the widget and domid is the id of the element in which the data will be placed
+var tweetConfig = {
+  "id": '569271594577850368',
+  "domId": 'example1',
+  "enableLinks": true
+};
+twitterFetcher.fetch(tweetConfig);
