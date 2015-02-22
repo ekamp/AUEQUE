cc1391a034f0d9c53b5e76062c6860fa69d83e68
ekamp <ekamp@eden.rutgers.edu>
2015-02-21 19:39:52 -0500

    Instagram addded

--- js/auequeInstagram.js
+++ js/auequeInstagram.js
@@ -1,8 +1,6 @@
-<script type="text/javascript">
-    var feed = new Instafeed({
-        get: 'user',
-        tagName: '227012267',
-        clientId: 'ee2c8fb2d5c24eab9054652cd7794526'
-    });
-    feed.run();
-</script>
+var feed = new Instafeed({
+	get: 'user',
+    tagName: '227012267',
+    clientId: 'ee2c8fb2d5c24eab9054652cd7794526'
+ });
+feed.run();
