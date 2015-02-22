0ae7d40ed6043d815be947918f9e79f380a7dcd7
ekamp <ekamp@eden.rutgers.edu>
2015-02-21 22:46:03 -0500

    Tumblr posts added to feed

--- js/auequeTumblr.js
+++ js/auequeTumblr.js
@@ -1,8 +1,7 @@
 //All information is stored within tumblr_api_read
 console.log(tumblr_api_read);
-
 //All the post information is stored within the posts portion of the object iterated thru and displayed below
 for (var i=0;i<tumblr_api_read.posts.length;i++) {
 	var postDate = tumblr_api_read.posts[i].date;
-	document.write('<a href="' + tumblr_api_read.posts[i].url + '">'+postDate+'</a>');
+	document.write('<a href="' + tumblr_api_read.posts[i].url + '">'+postDate+'</a><br/>');
 }
