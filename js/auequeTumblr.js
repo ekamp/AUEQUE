6cee3b7e9c594a09054f85f8c0d79d5588790d79
ekamp <ekamp@eden.rutgers.edu>
2015-02-21 20:38:52 -0500

    Attempting to add tumblr

--- js/auequeTumblr.js
+++ js/auequeTumblr.js
@@ -1,2 +1,3 @@
 //TODO filter the data so that it can be displayed
-var tumblrRestBlogRequest = "http://api.tumblr.com/v2/blog/aueque.tumblr.com/posts?api_key=2vGFErqOzbNAiZwljkUDd0fu3ywY2fqt7dPXaKuA9i5OjUduA6";
+// var tumblrRestBlogRequest = "http://api.tumblr.com/v2/blog/aueque.tumblr.com/posts?api_key=2vGFErqOzbNAiZwljkUDd0fu3ywY2fqt7dPXaKuA9i5OjUduA6";
+document.write('<a href="' + tumblr_api_read['posts'][0]['url'] + '">Most Recent Tumblr Post</a>');
