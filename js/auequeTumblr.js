c04fea7a6ddeed4ade0d80d0dd4acc48e215b25f
ekamp <ekamp@eden.rutgers.edu>
2015-02-21 22:27:24 -0500

    Tumblr added further integration and polling for information tmr

--- js/auequeTumblr.js
+++ js/auequeTumblr.js
@@ -1,3 +1,3 @@
 //TODO filter the data so that it can be displayed
 // var tumblrRestBlogRequest = "http://api.tumblr.com/v2/blog/aueque.tumblr.com/posts?api_key=2vGFErqOzbNAiZwljkUDd0fu3ywY2fqt7dPXaKuA9i5OjUduA6";
-document.write('<a href="' + tumblr_api_read['posts'][0]['url'] + '">Most Recent Tumblr Post</a>');
+document.write('<a href="' + tumblr_api_read[1][0]['url'] + '">Most Recent Tumblr Post</a>');
