62d5217b1f64fec571ae2c1811259c4e1b28b40e
ekamp <ekamp@eden.rutgers.edu>
2015-02-21 22:45:03 -0500

    Testing

--- js/auequeTumblr.js
+++ js/auequeTumblr.js
@@ -3,5 +3,6 @@ console.log(tumblr_api_read);
 
 //All the post information is stored within the posts portion of the object iterated thru and displayed below
 for (var i=0;i<tumblr_api_read.posts.length;i++) {
-	document.write('<a href="' + tumblr_api_read.posts[i].url + '">tumblr_api_read.posts[i].date</a>');
+	var postDate = tumblr_api_read.posts[i].date;
+	document.write('<a href="' + tumblr_api_read.posts[i].url + '">'+postDate+'</a>');
 }
