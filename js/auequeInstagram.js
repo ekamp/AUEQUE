cc84bd0a6396c739944f13a97a26fc958c9ccd7d
ekamp <ekamp@eden.rutgers.edu>
2015-02-21 19:36:57 -0500

    Instagram addded

---
+++ js/auequeInstagram.js
@@ -0,0 +1,8 @@
+<script type="text/javascript">
+    var feed = new Instafeed({
+        get: 'user',
+        tagName: '227012267',
+        clientId: 'ee2c8fb2d5c24eab9054652cd7794526'
+    });
+    feed.run();
+</script>
--- js/auequeTumblr.js
+++ js/auequeTumblr.js
@@ -1,3 +1,2 @@
 //TODO filter the data so that it can be displayed
 var tumblrRestBlogRequest = "http://api.tumblr.com/v2/blog/aueque.tumblr.com/posts?api_key=2vGFErqOzbNAiZwljkUDd0fu3ywY2fqt7dPXaKuA9i5OjUduA6";
-
