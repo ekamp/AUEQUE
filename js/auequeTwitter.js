cb1d4134abc3302194dbdeb220a44e8a2c5a9b5d
ekamp <ekamp@eden.rutgers.edu>
2015-02-21 19:00:21 -0500

    Altered div population name

--- js/auequeTwitter.js
+++ js/auequeTwitter.js
@@ -2,7 +2,7 @@
 //id is the blog id collected from the widget and domid is the id of the element in which the data will be placed
 var tweetConfig = {
   "id": '569271594577850368',
-  "domId": 'example1',
+  "domId": 'tweetList',
   "enableLinks": true
 };
 twitterFetcher.fetch(tweetConfig);
