6ec2c854dc636c4d22baf0168ab10aa6a0bb810d
ekamp <ekamp@eden.rutgers.edu>
2015-03-06 20:46:50 -0500

    Icon bar now changes with click function

--- js/aueque.js
+++ js/aueque.js
@@ -16,11 +16,10 @@ window.onload = function(){
 /** OnClick functions **/
 
 function homeIconClicked(){
-	iconNavigationBar.className = "icon-bar five-up icon-bar-blog-about";
+	iconNavigationBar.className = "icon-bar four-up icon-bar-blog-about";
 }
 
 function twitterIconClicked(){
-	alert("clicking twitter icon");
 	iconNavigationBar.className = "icon-bar four-up icon-bar-twitter";
 }
 
