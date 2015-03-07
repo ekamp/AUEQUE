c21c18dc1fa2042c718f61d79cb8992e3dcdf09b
ekamp <ekamp@eden.rutgers.edu>
2015-03-06 20:27:05 -0500

    Adding click listeners in order to change navigation bar color

--- js/aueque.js
+++ js/aueque.js
@@ -1,32 +1,33 @@
 window.onload = function(){
 	//Bind the objects
-var iconNavigationBar = document.getElementById("iconNavigationBar"),
-	homeNavigationItem = document.getElementById("homeNavItem"),
-	twitterNavigationItem = document.getElementById("twitterNavItem"),
-	instagramNavigationItem = document.getElementById("instagramNavItem"),
-	aboutNavigationItem = document.getElementById("aboutNavItem");
+	var iconNavigationBar = document.getElementById("iconNavigationBar"),
+		homeNavigationItem = document.getElementById("homeNavItem"),
+		twitterNavigationItem = document.getElementById("twitterNavItem"),
+		instagramNavigationItem = document.getElementById("instagramNavItem"),
+		aboutNavigationItem = document.getElementById("aboutNavItem");
 
-//Apply the click listeners
-homeNavigationItem.addEventListener("click",homeIconClicked,false);
-twitterNavigationItem.addEventListener("click",twitterIconClicked,false);
-instagramNavigationItem.addEventListener("click",instagramIconClicked,false);
-aboutNavigationItem.addEventListener("click",aboutIconClicked,false);	
+	//Apply the click listeners
+	homeNavigationItem.addEventListener("click",homeIconClicked,false);
+	twitterNavigationItem.addEventListener("click",twitterIconClicked,false);
+	instagramNavigationItem.addEventListener("click",instagramIconClicked,false);
+	aboutNavigationItem.addEventListener("click",aboutIconClicked,false);	
 }
 
-//Create the click functions
+/** OnClick functions **/
+
 function homeIconClicked(){
-	alert("clicking home icon");
-	document.getElementById("iconNavigationBar").className = "icon-bar four-up icon-bar-blog-about";
+	iconNavigationBar.className = "icon-bar four-up icon-bar-blog-about";
 }
 
 function twitterIconClicked(){
-	document.getElementById("iconNavigationBar").className = "icon-bar four-up icon-bar-twitter";
+	alert("clicking twitter icon");
+	iconNavigationBar.className = "icon-bar four-up icon-bar-twitter";
 }
 
 function instagramIconClicked(){
-	document.getElementById("iconNavigationBar").className = "icon-bar four-up icon-bar-instagram";
+	iconNavigationBar.className = "icon-bar four-up icon-bar-instagram";
 }
 
 function aboutIconClicked(){
-	document.getElementById("iconNavigationBar").className = "icon-bar four-up icon-bar-blog-about";
+	iconNavigationBar.className = "icon-bar four-up icon-bar-blog-about";
 }
