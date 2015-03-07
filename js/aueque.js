dba40743221cdf42e29a8211928a2cd43fa377f9
ekamp <ekamp@eden.rutgers.edu>
2015-03-06 21:08:03 -0500

    Active classes added

--- js/aueque.js
+++ js/aueque.js
@@ -1,10 +1,12 @@
+var iconNavigationBar, homeNavigationItem ,twitterNavigationItem ,instagramNavigationItem ,aboutNavigationItem;
+
 window.onload = function(){
 	//Bind the objects
-	var iconNavigationBar = document.getElementById("iconNavigationBar"),
-		homeNavigationItem = document.getElementById("homeNavItem"),
-		twitterNavigationItem = document.getElementById("twitterNavItem"),
-		instagramNavigationItem = document.getElementById("instagramNavItem"),
-		aboutNavigationItem = document.getElementById("aboutNavItem");
+	iconNavigationBar = document.getElementById("iconNavigationBar");
+	homeNavigationItem = document.getElementById("homeNavItem");
+	twitterNavigationItem = document.getElementById("twitterNavItem");
+	instagramNavigationItem = document.getElementById("instagramNavItem");
+	aboutNavigationItem = document.getElementById("aboutNavItem");
 
 	//Apply the click listeners
 	homeNavigationItem.addEventListener("click",homeIconClicked,false);
@@ -16,21 +18,32 @@ window.onload = function(){
 /** OnClick functions **/
 
 function homeIconClicked(){
+	clearItemNavigationFormatting();
 	iconNavigationBar.className = "icon-bar four-up icon-bar-blog-about";
-	document.getElementById("homeNavItem").className = "item active";
+	homeNavigationItem.className = "item active";
 }
 
 function twitterIconClicked(){
+	clearItemNavigationFormatting();
 	iconNavigationBar.className = "icon-bar four-up icon-bar-twitter";
-	document.getElementById("twitterNavItem").className = "item active";
+	twitterNavigationItem.className = "item active";
 }
 
 function instagramIconClicked(){
+	clearItemNavigationFormatting();
 	iconNavigationBar.className = "icon-bar four-up icon-bar-instagram";
-	document.getElementById("instagramNavItem").className = "item active";
+	instagramNavigationItem.className = "item active";
 }
 
 function aboutIconClicked(){
+	clearItemNavigationFormatting();
 	iconNavigationBar.className = "icon-bar four-up icon-bar-blog-about";
-	document.getElementById("aboutNavItem").className = "item active";
+	aboutNavigationItem.className = "item active";
+}
+
+function clearItemNavigationFormatting(){
+	homeNavigationItem.className = "item";
+	twitterNavigationItem.className = "item";
+	instagramNavigationItem.className = "item";
+	aboutNavigationItem.className = "item";
 }
