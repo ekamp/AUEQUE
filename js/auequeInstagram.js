46d07d898d6eec9b0128d975553657494ef41d27
ekamp <ekamp@eden.rutgers.edu>
2015-04-17 19:25:05 -0400

    Formatting the instagram page

--- js/auequeInstagram.js
+++ js/auequeInstagram.js
@@ -2,8 +2,8 @@ function fetchInstagramPosts(){
 	var feed = new Instafeed({
 		get: 'user',
 		userId: 227012267,
-		accessToken: '283823831.ee2c8fb.d8276e2ea678489f9a2d283b720dc64a'
-		template: '<a class=\x22instagram\x22 href=\x22{{link}}\x22><img src=\x22{{image}}\x22 /></a>'
+		accessToken: '283823831.ee2c8fb.d8276e2ea678489f9a2d283b720dc64a',
+		template: '<a class="instagram" href="{{link}}""><img src="{{image}}" /></a>'
 	});
 	feed.run();	
 }
