eb9b3179517a1dabfee1262e16e899133963e652
ekamp <ekamp@eden.rutgers.edu>
2015-04-18 10:58:47 -0400

    Passing hooks and url as strings now

--- js/auequeInstagram.js
+++ js/auequeInstagram.js
@@ -4,7 +4,7 @@ function fetchInstagramPosts(){
 		userId: 227012267,
 		accessToken: '283823831.ee2c8fb.d8276e2ea678489f9a2d283b720dc64a',
 		resolution: 'low_resolution',
-		template: '<img onclick="OpenUrlInMobileOrWebpage("{{link}}","{{id}}")" class="instagram" src="{{image}}"/>'
+		template: '<img onclick="OpenUrlInMobileOrWebpage('{{link}}','{{id}}')" class="instagram" src="{{image}}"/>'
 	});
 	feed.run();	
 }
